54e3f8f6c1742ef632f809e7b644adca6d7a227d
Tom Gosling <tom@pippin.eu>
2016-05-03 02:39:24 +0100

    User resource

---
+++ web/controllers/user_controller.ex
@@ -0,0 +1,31 @@
+defmodule Tarantino.UserController do
+  use Tarantino.Web, :controller
+  alias Tarantino.User
+
+  def index(conn, _params) do
+    users = Repo.all(Tarantino.User)
+    render conn, "index.html", users: users
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(Tarantino.User, id)
+    render conn, "show.html", user: user
+  end
+
+  def new(conn, _params) do
+    changeset = User.changeset(%User{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = User.changeset(%User{}, user_params)
+    case Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+end

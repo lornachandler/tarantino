9fe51e5fb1014a71e7dd881782e44fcd163080c5
Tom Gosling <tom@pippin.eu>
2016-05-09 13:17:40 +0100

    Permalinks and categories resources and slug extension

--- web/models/video.ex
+++ web/models/video.ex
@@ -1,17 +1,20 @@
 defmodule Tarantino.Video do
   use Tarantino.Web, :model
 
+  @primary_key {:id, Tarantino.Permalink, autogenerate: true}
   schema "videos" do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Tarantino.User
+    belongs_to :category, Tarantino.Category
 
     timestamps
   end
 
   @required_fields ~w(url title description)
-  @optional_fields ~w()
+  @optional_fields ~w(category_id)
 
   @doc """
   Creates a changeset based on the `model` and `params`.
@@ -22,5 +25,27 @@ defmodule Tarantino.Video do
   def changeset(model, params \\ :empty) do
     model
     |> cast(params, @required_fields, @optional_fields)
+    |> slugify_title()
+    |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+end
+
+defimpl Phoenix.Param, for: Tarantino.Video do
+  def to_param(%{slug: slug, id: id}) do
+    "#{id}-#{slug}"
   end
 end
